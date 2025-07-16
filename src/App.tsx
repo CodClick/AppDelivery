9cdd98722e6c434efd0f5f3a21b65a2a90e315e8
CodClick <codclick.marketing@gmail.com>
2025-07-16 11:14:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,7 @@ const App = () => (
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/checkout" element={<Checkout />} />
+              <Route path="/admin-register" element={<AdminRegister />} />
                <Route
                   path="/"
                   element={
