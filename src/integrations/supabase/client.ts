ab92ff1725f84dd95653650d0d2e7b202623b0a6
CodClick <codclick.marketing@gmail.com>
2025-07-20 16:29:41 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ypsfoumafbfgcvrxylmw.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwc2ZvdW1hZmJmZ2N2cnh5bG13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NjkwODQsImV4cCI6MjA2MDM0NTA4NH0.aYVYGIcTbPeYONtCQaO543wfLstNDGBiUw-dUbNf6Xs";
+const SUPABASE_URL = "https://gjwmswafmuyhobwhuwup.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdqd21zd2FmbXV5aG9id2h1d3VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTgwODksImV4cCI6MjA2NTc3NDA4OX0.GGssWKxMhTggo0yGQpVArjulEiI9FSWUNitxqfCQjTw";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
