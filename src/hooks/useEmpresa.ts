882b18d5b46425a5ce7a05596298bf86454032d0
CodClick <codclick.marketing@gmail.com>
2025-07-20 07:03:12 -0300

    Update useEmpresa.ts

--- src/hooks/useEmpresa.ts
+++ src/hooks/useEmpresa.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "@/services/supabaseClient";
+import { supabase } from '@/services/supabaseService';
 import { useAuth } from "@/hooks/useAuth";
 
 interface Empresa {
