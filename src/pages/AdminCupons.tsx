8e0b34c543a05d94b2720f1c4e102e8ce947d41f
CodClick <codclick.marketing@gmail.com>
2025-07-24 15:25:41 -0300

    Update AdminCupons.tsx

--- src/pages/AdminCupons.tsx
+++ src/pages/AdminCupons.tsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
 
-export default function AdminCupons() {
+  function AdminCupons() {
   const [cupons, setCupons] = useState<any[]>([]);
   const [open, setOpen] = useState(false);
   const [formData, setFormData] = useState({
