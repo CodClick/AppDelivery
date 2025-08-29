3f3999dea6343dbf0f6e5e8424828b4dad662b20
CodClick <codclick.marketing@gmail.com>
2025-08-29 20:28:43 -0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { supabase } from "./supabaseClient"; // Importa o cliente Supabase
 
-// Função para combinar classes Tailwind de forma inteligente
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function getSupabaseImageUrl(filePath: string | null | undefined): string {
-  if (!filePath) {
-    return "";
-  }
-  const cleanPath = filePath.startsWith("menu_images/") ? filePath.substring("menu_images/".length) : filePath;
-  
-  const { data } = supabase.storage.from("menu_images").getPublicUrl(cleanPath);
-  
-  return data.publicUrl;
-}
-
-export const formatCurrency = (value: number) => {
-  return new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
   }).format(value);
-};
+}
