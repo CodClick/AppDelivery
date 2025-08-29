efad59b0790a055b45b9716522d92937b044031c
CodClick <codclick.marketing@gmail.com>
2025-08-29 12:24:17 -0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
 
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase URL or anonymous key environment variables");
 }
 
-export function formatCurrency(value: number): string {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export function getSupabaseImageUrl(filePath: string | null | undefined): string {
+  if (!filePath) {
+    return "";
+  }
+  // Remove o nome do bucket do caminho, se ele já estiver presente
+  const cleanPath = filePath.startsWith("menu_images/") ? filePath.substring("menu_images/".length) : filePath;
+  
+  const { data } = supabase.storage.from("menu_images").getPublicUrl(cleanPath);
+  
+  return data.publicUrl;
+}
+
+export const formatCurrency = (value: number) => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
   }).format(value);
-}
+};
