40dfe8b7f4f7e78242ae0540ca4da4951fafab71
CodClick <codclick.marketing@gmail.com>
2025-08-29 12:59:53 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,11 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Acessa as variáveis de ambiente a partir do arquivo .env.local
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase URL or anonymous key environment variables");
-}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
