1bd60d6dd998ec079e80feff6255e86f54ab5660
CodClick <codclick.marketing@gmail.com>
2025-08-29 12:55:10 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("Lendo variáveis de ambiente...");
-console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("Supabase Anon Key:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "Carregada" : "Não Carregada");
-
+// Acessa as variáveis de ambiente a partir do arquivo .env.local
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
