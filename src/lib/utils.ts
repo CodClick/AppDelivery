1d863af6004f4581866db0e93e9ae6928a77237a
CodClick <codclick.marketing@gmail.com>
2025-08-29 12:59:23 -0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "./supabaseClient"; // Importa o cliente Supabase
 
 // Função para combinar classes Tailwind de forma inteligente
 export function cn(...inputs: ClassValue[]) {
@@ -11,15 +11,7 @@ export function getSupabaseImageUrl(filePath: string | null | undefined): string
   if (!filePath) {
     return "";
   }
-  // Remove o nome do bucket do caminho, se ele já estiver presente
   const cleanPath = filePath.startsWith("menu_images/") ? filePath.substring("menu_images/".length) : filePath;
-
-  // Removido a criação do cliente aqui
-  // O cliente será passado como parâmetro
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
   
   const { data } = supabase.storage.from("menu_images").getPublicUrl(cleanPath);
   
