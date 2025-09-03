d9aad679807d09bd8e85a1436c4ac2704e41b73d
CodClick <codclick.marketing@gmail.com>
2025-09-02 23:19:48 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider, useAuth } from "@/hooks/useAuth.tsx";
+import { AuthProvider, useAuth } from "./hooks/useAuth.tsx";
 import { CartProvider } from "@/contexts/CartContext";
 import { EmpresaProvider } from "@/contexts/EmpresaContext";
 
