c90c5e57017e7c6b1c9030bb4eb68eb0c451301e
CodClick <codclick.marketing@gmail.com>
2025-09-03 00:21:21 -0300

    Update AppLayout.tsx

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,44 +1,24 @@
-import { Outlet } from "react-router-dom"; // Importe o Outlet
 import { useAuth } from "@/hooks/useAuth";
-import { useEmpresa } from "@/contexts/EmpresaContext"; // Adicione a importação do useEmpresa
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
-import MainNav from '@/components/MainNav';
-import UserNav from '@/components/UserNav';
-import TeamSwitcher from '@/components/TeamSwitcher';
+import { Link, Outlet } from "react-router-dom"; // Importe o Outlet
 
 const AppLayout = () => {
-  const { currentUser } = useAuth();
-  const { empresa, loading, error } = useEmpresa(); // Use o hook da empresa
-
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
-
-  if (error || !empresa) {
-    return <div>Ooops... Empresa não encontrada!</div>;
-  }
+  const { currentUser, logOut } = useAuth();
 
   return (
-    <div className="flex flex-col">
-      <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-          <TeamSwitcher slug={empresa?.slug} />
-          <MainNav className="mx-6 hidden md:block" />
-          <div className="ml-auto flex items-center space-x-4">
-            {currentUser ? (
-              <UserNav />
-            ) : (
-              <Button asChild>
-                <Link to="/login">Entrar</Link>
-              </Button>
-            )}
-          </div>
-        </div>
+    <div>
+      <div className="fixed top-4 right-4 z-50">
+        {currentUser ? (
+          <Button onClick={logOut} variant="outline">
+            Sair
+          </Button>
+        ) : (
+          <Button asChild>
+            <Link to="/login">Entrar</Link>
+          </Button>
+        )}
       </div>
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <Outlet /> {/* ESTE É O COMPONENTE CHAVE */}
-      </div>
+      <Outlet /> {/* <-- O COMPONENTE CHAVE ESTÁ AQUI */}
     </div>
   );
 };
