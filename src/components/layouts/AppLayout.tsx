a3883a06fc2af8890a8bfbfacff5c192c7bf5e3e
CodClick <codclick.marketing@gmail.com>
2025-09-03 22:48:36 -0300

    Update AppLayout.tsx

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,18 +1,25 @@
 import { useAuth } from "@/hooks/useAuth";
 import { Button } from "@/components/ui/button";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom"; // Importe 'useNavigate'
 import ShoppingCart from "@/components/ShoppingCart";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 
 const AppLayout = () => {
   const { currentUser, logOut } = useAuth();
+  const navigate = useNavigate(); // Inicialize o hook de navegação
+
+  // Função para lidar com o logout
+  const handleLogout = async () => {
+    await logOut();
+    navigate("/login"); // Redireciona para a página de login após o logout
+  };
 
   return (
     <div>
       <div className="fixed top-4 right-4 z-50">
         {currentUser ? (
-          <Button onClick={logOut} variant="outline">
+          <Button onClick={handleLogout} variant="outline"> {/* Chame a nova função */}
             Sair
           </Button>
         ) : (
@@ -22,7 +29,6 @@ const AppLayout = () => {
         )}
       </div>
       <Outlet />
-      {/* Mova os componentes globais para o layout */}
       <ShoppingCart />
       <Toaster />
       <Sonner />
